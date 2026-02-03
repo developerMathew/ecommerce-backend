5b707d3a3e112e885c1fe93956812a6da784b841
Mathew Josephraj <developermathew2002@gmail.com>
2026-02-03 19:47:33 +0530

    backend initial commit

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,23 +1,37 @@
-const Product = require("../model/productModel");
+const mongoose = require("mongoose");
+const Product = require("../models/productModel");
 
-exports.createProduct = async (req, res) => {
+const createProduct = async (req, res) => {
   try {
-    const { name, price, stock, image, seller } = req.body;
-
-    if (!seller) {
-      return res.status(400).json({ message: "Seller id required" });
-    }
-
-    const product = await Product.create({
-      name,
-      price,
-      stock,
-      image,
-      seller
-    });
-
+    const product = await Product.create(req.body);
     res.status(201).json(product);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
+
+const getallProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(201).json(products);
+  }
+  catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+}
+const getSingleProducts = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+
+module.exports = { createProduct, getallProducts, getSingleProducts };
