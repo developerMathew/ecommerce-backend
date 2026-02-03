715ae5f3a784af8a42944c5bdb801c45a93314d0
Mathew Josephraj <developermathew2002@gmail.com>
2026-02-03 15:51:36 +0530

    backend initial commit

--- routes/sellerRoutes.js
+++ routes/sellerRoutes.js
@@ -2,5 +2,5 @@ const express = require('express');
 const { registerSeller, loginSeller } = require('../controllers/sellerController');
 const route = express.Router();
 
-route.post('/', registerSeller)
-route.post('/', loginSeller)
+route.post('/register', registerSeller)
+route.post('/login', loginSeller)
